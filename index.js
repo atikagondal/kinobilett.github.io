630a97206790fa21771b7f5054b83454995feb2b
Atika Gondal <atikagondal@yahoo.com>
2024-02-09 22:13:18 +0100

    ny3

--- index.js
+++ index.js
@@ -2,78 +2,77 @@
 let bilettliste = [];
 
 // her lages funksjonen for å kjøpe bilett
-    function kjopBillett() {
-        // Henter verdier fra input-feltene
-        let feilmelding=0;
-        let valg = document.getElementById("filmDropdown").value;
-        let antall = document.getElementById("antall").value;
-        let fornavn = document.getElementById("fornavn").value;
-        let etternavn = document.getElementById("etternavn").value;
-        let telefon = document.getElementById("tlf").value;
-        let epost = document.getElementById("epost").value;
+function kjopBillett() {
+    // Henter verdier fra input-feltene
+    let valg = document.getElementById("filmDropdown").value;
+    let antall = document.getElementById("antall").value;
+    let fornavn = document.getElementById("fornavn").value;
+    let etternavn = document.getElementById("etternavn").value;
+    let telefon = document.getElementById("tlf").value;
+    let epost = document.getElementById("epost").value;
 
-        //oppretter billettobjektet
-        let billett = {
-            valg: valg,
-            antall: antall,
-            fornavn: fornavn,
-            etternavn: etternavn,
-            telefon: telefon,
-            epost: epost
-        };
-    
-    
+    //oppretter billettobjektet
+    let billett = {
+        valg: valg,
+        antall: antall,
+        fornavn: fornavn,
+        etternavn: etternavn,
+        telefon: telefon,
+        epost: epost
+    };
 
+    console.log("KjopBillett(): " + billett);
 
-        console.log("KjopBillett(): " + billett);
+    //Definerer feilmelding variabel
+    let feilmelding = document.getElementById("feilmelding");
 
-        //Validere input (implementere valideringsfunksjon for hvert felt)
-    if (!antall || isNaN(parseInt(antall)) || parseInt(antall) <= 0) {
-        feilmeldingElement.innerText = 'Vennligst fyll ut alle felt før du kjøper bilett';
-        return;
+    //Validere input (implementere valideringsfunksjon for hvert felt)
+    if (!valg || !antall || isNaN(parseInt(antall)) || parseInt(antall) <= 0) {
+        feilmelding.innerText = 'Vennligst fyll ut alle felt før du kjøper bilett';
     }
     // Validering for fornavn og etternavn (kun bokstaver tillatt)
     let navnRegex = /^[a-zA-Z]+$/;
     if (!fornavn || !navnRegex.test(fornavn) || !etternavn || !navnRegex.test(etternavn)) {
-        feilmeldingElement.innerText = 'Vennligst skriv inn gyldige navn (kun bokstaver tillatt).';
-        return;
+        feilmelding.innerText = 'Vennligst skriv inn gyldige navn (kun bokstaver tillatt).';
     }
 
     // Validering for telefonnummer (kun tall tillatt)
     let telefonRegex = /^\d+$/;
     if (!telefon || !telefonRegex.test(telefon)) {
-        feilmeldingElement.innerText = 'Vennligst skriv inn et gyldig telefonnummer (kun tall tillatt).';
-        return;
+        feilmelding.innerText = 'Vennligst skriv inn et gyldig telefonnummer (kun tall tillatt).';
     }
 
     // Validering for e-postadresse
     let epostRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!epost || !epostRegex.test(epost) || !gyldigEpost(epost)) {
-        feilmeldingElement.innerText = 'Vennligst skriv inn en gyldig e-postadresse.';
-        return;
+    if (!epost || !epostRegex.test(epost)) {
+        feilmelding.innerText = 'Vennligst skriv inn en gyldig e-postadresse.';
     }
 
-
-    function visAllebilletter() {
-        let billettlisteElement = document.getElementById("billettliste");
-        billettlisteElement.innerHTML = ""; // Fjerner eksisterende innhold før vi legger til nytt
-    
-        let ut = "<table> <tr> " +
-            "<th>Velg film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
-        for (let billett of bilettliste) {
-            ut += "<tr>";
-            ut += "<td>" + billett.valg + "</td><td>" + billett.antall + "</td><td>" + billett.fornavn + "</td><td>" + billett.etternavn + "</td><td>" + billett.telefon
-                + "</td><td>" + billett.epost + "</td>";
-            ut += "</tr>";
-        }
-        ut += "</table>";
-    
-        billettlisteElement.innerHTML = ut; // Legger til tabellen med billettinformasjonen på nettsiden
+    if (feilmelding.innerText === '') {
+        bilettliste.push(billett);
+        console.log(bilettliste + " tidlig");
+        TomUtAlleBilletter();
     }
+    visAllebilletter();
 }
-    
 
+function visAllebilletter() {
+    let billettlisteElement = document.getElementById("billettliste");
+    billettlisteElement.innerHTML = ""; // Fjerner eksisterende innhold før vi legger til nytt
 
+    let ut = "<table> <tr> " +
+        "<th>Velg film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
+    for (let billett of bilettliste) {
+        ut += "<tr>";
+        ut += "<td>" + billett.valg + "</td><td>" + billett.antall + "</td><td>" + billett.fornavn + "</td><td>" + billett.etternavn + "</td><td>" + billett.telefon
+            + "</td><td>" + billett.epost + "</td>";
+        ut += "</tr>";
+    }
+    ut += "</table>";
+
+    billettlisteElement.innerHTML = ut; // Legger til tabellen med billettinformasjonen på nettsiden        }
+
+}
 
 function TomUtAlleBilletter() {
     document.getElementById("filmDropdown").value="--Select a film--"
@@ -89,4 +88,3 @@ function slettAlleBiletter() {
     console.log("hei");
     document.getElementById("billettliste").innerHTML = "";
 }
-    
---
+++ kino.js
@@ -0,0 +1,105 @@
+// her lager vi en javascript kode som skal knyttes sammen med html filen
+
+//først lages en variabel som jeg lagrer billetter i
+let bilettliste = [];
+
+let feilmeldingElement = document.getElementById('feilmelding')
+
+
+// her lages funksjonen for å kjøpe bilett
+function kjopbilett() {
+    //Henter verdier fra input feltet
+    let antall = document.getElementsByName('antall')[0].value;
+    let fornavn = document.getElementById('navn').value;
+    let etternavn = document.getElementsByName('etternavn')[0].value;
+    let telefon = document.getElementsByName('telefon')[0].value;
+    let epost = document.getElementsByName('epost')[0].value;
+
+    //Validere input (implementere valideringsfunksjon for hvert felt)
+    if (!antall || isNaN(parseInt(antall)) || parseInt(antall) <= 0) {
+        feilmeldingElement.innerText = 'Vennligst fyll ut alle felt før du kjøper bilett';
+        return;
+    }
+    // Validering for fornavn og etternavn (kun bokstaver tillatt)
+    let navnRegex = /^[a-zA-Z]+$/;
+    if (!fornavn || !navnRegex.test(fornavn) || !etternavn || !navnRegex.test(etternavn)) {
+        feilmeldingElement.innerText = 'Vennligst skriv inn gyldige navn (kun bokstaver tillatt).';
+        return;
+    }
+
+    // Validering for telefonnummer (kun tall tillatt)
+    let telefonRegex = /^\d+$/;
+    if (!telefon || !telefonRegex.test(telefon)) {
+        feilmeldingElement.innerText = 'Vennligst skriv inn et gyldig telefonnummer (kun tall tillatt).';
+        return;
+    }
+
+    // Validering for e-postadresse
+    let epostRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!epost || !epostRegex.test(epost) || !gyldigEpost(epost)) {
+        feilmeldingElement.innerText = 'Vennligst skriv inn en gyldig e-postadresse.';
+        return;
+    }
+
+    //opprett billettobjektet
+    let billett = {
+        film: document.getElementById('filmDropdown').value,
+        antall: antall,
+        fornavn: fornavn,
+        etternavn: etternavn,
+        telefon: telefon,
+        epost: epost
+    };
+
+    //legger til billetter i listen med push
+    bilettliste.push(billett);
+    // så oppdaterer vi listen av billetten
+    visbillettliste();
+
+    //for å tømme input feltene
+    document.getElementsByName('antall')[0].value = '';
+    document.getElementById('navn').value = '';
+    document.getElementsByName('etternavn')[0].value = '';
+    document.getElementsByName('telefon')[0].value = '';
+    document.getElementById('epost').value = '';
+
+}
+
+//funksjon for å vise listen av billettene
+function visbillettliste() {
+    let elementliste = document.getElementById('billettliste');
+    elementliste.innerHTML = '';
+
+    for (let i = 0; i < bilettliste.length; i++) {
+        let billett = bilettliste[i];
+        let billettTekst = 'Film: '+ billett.film +',Antall: ' + billett.antall + ', Fornavn: ' + billett.fornavn + ' , Etternavn: ' + billett.etternavn + ', Telefon: ' + billett.telefon + ', Epost: ' + billett.epost;
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(billettTekst));
+        elementliste.appendChild(li);
+    }
+}
+
+// så har vi funksjonen til å slette alle bilettene
+function slettAlleBiletter() {
+    bilettliste = [];
+    visbillettliste();
+}
+
+//Validering av epost
+function gyldigEpost(epost) {
+    let epostRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return epostRegex.test(epost);
+}
+
+const fields = ['Fornavn', 'Etternavn', 'Telefon', 'Epost'];
+
+for (let i = 0; i < fields.length; i++) {
+    let inputField = document.createElement('input');
+    inputField.type = 'text';
+    inputField.placeholder = fields[i];
+    inputField.id = fields[i].toLowerCase();
+
+    let label = document.createElement('label');
+    label.textContent = fields[i] + ':';
+
+}
